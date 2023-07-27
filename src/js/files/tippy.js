1260fbe533d2ce12bb70fdf6466c93d5a3fe5812
Artiom_Sokolov <sokolova-net@mail.ru>
2023-07-27 14:33:00 +0300

    section steps

--- src/js/files/tippy.js
+++ src/js/files/tippy.js
@@ -11,7 +11,13 @@ import "../../scss/libs/tippy.scss";
 // Подключение cтилей из node_modules
 //import 'tippy.js/dist/tippy.css';
 
-// Запускаем и добавляем в объект модулей
-flsModules.tippy = tippy('[data-tippy-content]', {
+const ongoing = document.querySelector('#ongoing');
 
+// Запускаем и добавляем в объект модулей
+flsModules.tippy = tippy('[data-tippy-ongoing]', {
+  content: ongoing,
+  allowHTML: true,
+  interactive: true,
+  arrow: false,
 });
+
